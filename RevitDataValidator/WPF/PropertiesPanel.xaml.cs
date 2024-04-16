14c1cfe87e36e40e974f92cf948d0c5d3b1b2fea
Harry Mattison <harrymattison@gmail.com>
2024-04-16 17:29:08 -0400

    Scrolling improvement

--- RevitDataValidator/WPF/PropertiesPanel.xaml.cs
+++ RevitDataValidator/WPF/PropertiesPanel.xaml.cs
@@ -201,5 +201,12 @@ namespace RevitDataValidator
 
             }
         }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
     }
 }
