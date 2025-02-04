73e48dd57c73b13244435e2b93eda25b575df72a
Harry Mattison <harrymattison@gmail.com>
2025-02-04 15:02:13 -0500

    RunInstaller command

---
+++ RunInstaller/RunInstaller/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+foreach (Process process in Process.GetProcessesByName("revit"))
+{
+    process.WaitForExit();
+}
+var startInfo = new ProcessStartInfo
+{
+    FileName = args[0],
+    Arguments = "",
+    CreateNoWindow = true,
+    UseShellExecute = false
+};
+Process.Start(startInfo);
