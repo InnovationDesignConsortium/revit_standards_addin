88d2832418f46d01d0b217fca30ed04758f0168a
Harry Mattison <harrymattison@gmail.com>
2025-07-11 17:12:08 -0400

    remove usings

--- RevitDataValidator/Forms/FormEnableDisabledRules.cs
+++ RevitDataValidator/Forms/FormEnableDisabledRules.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.CodeAnalysis;
-using RevitDataValidator.Classes;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RevitDataValidator.Forms
