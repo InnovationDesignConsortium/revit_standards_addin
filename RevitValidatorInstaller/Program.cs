ce757650c787d87bb0861298516cea0d72780006
Harry Mattison <harrymattison@gmail.com>
2025-08-31 17:47:57 -0400

    clean up old files missed by uninstall

--- RevitValidatorInstaller/Program.cs
+++ RevitValidatorInstaller/Program.cs
@@ -5,10 +5,12 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 internal class Program
 {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
     private static void Main(string[] args)
     {
         var filename = Path.Combine(Path.GetTempPath(), "RevitDataValidator.msi");
@@ -19,25 +21,22 @@ internal class Program
 
         var dll = typeof(Program).Assembly.Location;
         var dllPath = Path.GetDirectoryName(dll);
+        if (dllPath == null)
+        {
+            return;
+        }
         LogManager.Configuration = new XmlLoggingConfiguration(Path.Combine(dllPath, "NLog.config"));
         var logConfig = LogManager.Configuration;
         var targets = logConfig.AllTargets;
-
+        const string INSTALLER_NAME = "RevitValidatorInstaller";
         foreach (var target in targets)
         {
             if (target is FileTarget ft)
             {
-                ft.FileName = 
-
-                    string.Concat(ft.FileName.ToString()
-                        .Substring(0, ft.FileName.ToString().Length - 4),
-                        " INSTALL ", 
-                        DateTime.Now.ToString().Replace(":", "-")
-                            .Replace("/", "_"), ".log");
+                ft.FileName = "${tempdir}/" + INSTALLER_NAME + DateTime.Now.ToString().Replace(":", "-").Replace("/", "_") + ".log";
             }
         }
         LogManager.Configuration = logConfig;
-        const string INSTALLER_NAME = "RevitValidatorInstaller";
         var currentProcess = Process.GetCurrentProcess();
 
         var otherInstallerProcesses = Process.GetProcessesByName(INSTALLER_NAME)
@@ -62,16 +61,57 @@ internal class Program
                 Logger.Info($"Process {process.ProcessName} {process.Id} exited");
             }
         }
-        var startInfo = new ProcessStartInfo
+
+        var versions = new List<string> { "2023", "2024", "2025", "2026", "2027" };
+        var dir = Path.GetDirectoryName(dllPath);
+        var addinsFolder = Path.GetDirectoryName(dir);
+        var revitversion = new DirectoryInfo(dir).Name;
+
+        foreach (var version in versions)
         {
-            FileName = filename,
-            Arguments = "",
-            CreateNoWindow = true,
-            UseShellExecute = true
-        };
-        Logger.Info($"About to start {startInfo.FileName}");
-        var p = Process.Start(startInfo);
-        p?.WaitForExit();
-        Logger.Info($"Completed {startInfo.FileName}");
+            dir = dllPath.Replace(revitversion, version);
+            if (Directory.Exists(dir))
+            {
+                foreach (var file in Directory.GetFiles(dir))
+                {
+                    try
+                    {
+                        File.Delete(file);
+                        Logger.Info("Deleted " + file);
+                    }
+                    catch { }
+                }
+            }
+            var addinsFolderWithVersion = Path.Combine(addinsFolder, version);
+            var addinFile = Path.Combine(addinsFolderWithVersion, "RevitDataValidator.addin");
+            if (File.Exists(addinFile))
+            {
+                try
+                {
+                    File.Delete(addinFile);
+                    Logger.Info($"Deleted {addinFile}");
+                }
+                catch { }
+            }
+        }
+
+        if (File.Exists(filename))
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = filename,
+                Arguments = "",
+                CreateNoWindow = true,
+                UseShellExecute = true
+            };
+            Logger.Info($"About to start {startInfo.FileName}");
+            var p = Process.Start(startInfo);
+            p?.WaitForExit();
+            Logger.Info($"Completed {startInfo.FileName}");
+        }
+        else
+        {
+            Logger.Info(filename + " does not exist");
+        }
     }
 }
